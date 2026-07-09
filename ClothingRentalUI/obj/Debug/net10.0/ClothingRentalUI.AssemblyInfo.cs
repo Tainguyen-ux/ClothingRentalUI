e6651929bda3dc34eff375522adda47019b68d0c
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-10 06:59:40 +0700

    feat: initialize product index page and configure static web asset build pipeline

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09dba2b5d9d6d060e42ca84ab32531a6cbbc9d2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eb8c29ad955c05867f1bfe65808a48efdd13862")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
