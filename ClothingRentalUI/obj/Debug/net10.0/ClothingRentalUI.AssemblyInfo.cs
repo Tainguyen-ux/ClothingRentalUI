e99c466e68c66f44e187341c49adbb6d743d51d7
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-09 19:55:46 +0700

    chore: initialize project build artifacts and configuration files

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add9760ba0633b5f05cdf1c9c7a2dc4b73de2e7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5756caef2fbe296f96e8165452430f27164753e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
