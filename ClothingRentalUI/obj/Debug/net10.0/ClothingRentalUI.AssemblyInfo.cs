85e91e7019a568cde162645bfe4bb1a8f6f01f23
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-10 16:13:24 +0700

    chore: build and generate artifacts for ClothingRentalUI project

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+409682ba347b291a40e5baa7f44abe21c908036b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87e95d4a1fa04de321aefb7b4452262fcf3392c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
