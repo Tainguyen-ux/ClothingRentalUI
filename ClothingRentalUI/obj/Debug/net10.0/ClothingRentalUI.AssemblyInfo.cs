fa45d0d1def2d8038767d72ba144ff372b794d4d
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-10 22:27:37 +0700

    feat: implement order details page with status tracking and management actions

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ec2e649c864a2b8d4c6a563e7fffb693f761060")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203a2aa2053ddf6424d33d5d7d2f1dbf482081b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
