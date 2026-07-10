3a6146240e29b4fedba11bb2954902bdc81ab75e
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-10 19:12:18 +0700

    style: align all table headers to the left globally to match column content align

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f18310fe9338aef70ee54a3044e6507d29e4f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc83621213715ad8c8de2ca2642f9c9245cafdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
