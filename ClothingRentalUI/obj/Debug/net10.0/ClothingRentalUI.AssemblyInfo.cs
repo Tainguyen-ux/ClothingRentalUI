c7c3b522ec66de63eec2e2ff34d3568fec1304d4
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-11 19:40:14 +0700

    build: generate initial project artifacts and assembly information

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8e94a0101fcded52849010e27f493fdf39572de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7e604e7e0fb7c51be4f50c97c52609403943c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
