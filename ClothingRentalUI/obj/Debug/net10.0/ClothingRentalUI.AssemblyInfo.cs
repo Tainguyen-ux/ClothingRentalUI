aafd0cd2280a70b7acbcce511dc585bf61a9b71d
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-11 20:41:01 +0700

    build: generate project artifacts and assembly metadata for debug configuration

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e797fe447623c7133a561d4b997b41040e487b2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6709178cb5a8d8ba2c564ab362a0ed4da3d73b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
