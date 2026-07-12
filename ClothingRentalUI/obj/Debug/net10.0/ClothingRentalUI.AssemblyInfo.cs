d08cf8df5e0a614b5516762dc5decf7aec1a6e57
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-12 10:59:28 +0700

    feat: add SaleDetail Razor page to order management module

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a2399630842c1c03581a8361031f85a675d342")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c3f029311fcec06b2334ddaee3ea26ef576555")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
