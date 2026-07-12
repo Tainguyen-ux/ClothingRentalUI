7dff813ebaaca236d91244d708c70040cd6c9323
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-12 11:21:12 +0700

    feat: add create order page to rental UI

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d6b0d668f755afa1e30c4b9c8e799358f59810")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be87d25fb0e3af515bf2159d19968261b7a62ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
