34c243a354e9572c445e6423b865250671d50f62
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-13 11:09:43 +0700

    feat: add Login page code-behind file

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74adb1745248ca286390ccd5a2d6a737eb23e627")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb9ff4dfc3a09168d034a9c9daf7894d0d84261")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
