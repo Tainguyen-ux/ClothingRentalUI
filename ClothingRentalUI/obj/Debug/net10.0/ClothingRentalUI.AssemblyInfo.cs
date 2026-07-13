3cb9ff4dfc3a09168d034a9c9daf7894d0d84261
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-13 10:56:52 +0700

    feat: implement responsive order editing page with customer search and management features

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f7f1e37fe7a12320cf6893c37915fb45d9794b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74adb1745248ca286390ccd5a2d6a737eb23e627")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
