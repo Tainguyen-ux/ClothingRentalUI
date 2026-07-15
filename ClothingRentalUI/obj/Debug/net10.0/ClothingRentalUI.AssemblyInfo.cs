47486d6b5d765f18779bd22fbcf4a42b9280803c
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-15 19:25:00 +0700

    feat: add Create and Edit Razor pages to the Orders module

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a39859e61504faf9ac5462294a6403047156bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b764160eba2c0399a6563ac7cedc9d56a628d52b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
