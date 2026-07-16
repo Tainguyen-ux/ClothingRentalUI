00670b36414ac9a2c41ab5af35a77920ae489b9c
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-16 07:41:34 +0700

    feat: add Liquidation page and configure Entity Framework and MiniExcel dependencies

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d2d582014259e5f70ff0bad0236f285784b1a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7023bcaf8fd70d7908ddb8908bcdc301f2a576a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
