588533323d73a747b7392cc3166a20c101003d87
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-17 16:22:59 +0700

    feat: add Edit page for managing orders with responsive layout and real-time customer search functionality

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb448b25bcf4b8d48fceaaea58118e2e0459c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad76e93a7da7b988959e7d0a710ab7550c74a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
