e2cfd2f86cf54229d6e947358dce1b3f87ee0e94
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-21 18:24:15 +0700

    feat: add product index page with filter functionality and card-based layout

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+752c4fb5ef124d6c9eea4f09bb834e7543309929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34dc87ee1ec9e343ce7bcb1e434c9b9b32828d4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
