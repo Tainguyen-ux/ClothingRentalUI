a2dfc7245a02e6ec6cf88994e822966d7381acdd
NguyenTai-Ux <nhox9xy@gmail.com>
2026-07-21 19:22:20 +0700

    feat: center barcode horizontally and vertically on high-DPI print canvas

--- ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
+++ ClothingRentalUI/obj/Debug/net10.0/ClothingRentalUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299e952263b775f0b7a058214fa7810d07603066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e44fec650d2194d301ec305408e86ace9fb8ad8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClothingRentalUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
